1253461f4ed26bf19648c6bb9499ce214f623ed7
Roylee-ML <roylee.stillway@gmail.com>
2016-07-18 13:38:05 +0800

    Initial Commit

---
+++ RLStickyHeaderFlowLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RLStickyHeaderFlowLayout
+//
+//  Created by Roy lee on 16/7/18.
+//  Copyright © 2016年 Roy lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
